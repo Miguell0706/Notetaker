3540742072eebdb6ba84de2e9cf9f0c97cb1951c
Miguel <Miguellozano3757@gmail.com>
2024-01-05 14:12:24 -0700

    sign in page worked on show button for password as well as some styling

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,7 +1,12 @@
+//This function is to get a random quote
 async function fetchRandomQuote() {
   try {
     const response = await fetch("https://api.quotable.io/random");
     const data = await response.json();
+    //get a new quote if the the quote exceeds more than 100 characters
+    if (data.content.length > 100) {
+      return fetchRandomQuote();
+    }
     return data.content;
   } catch (error) {
     console.error("Error fetching quote:", error);
@@ -15,3 +20,20 @@ document.addEventListener("DOMContentLoaded", async function () {
   // Display the quote
   quoteElement.textContent = quote;
 });
+//This function is to show and hide the password
+function showPassword() {
+    var passwordInput = document.getElementById('password');
+    var eyeOpen = document.getElementById('eye-open');
+    var eyeClosed = document.getElementById('eye-closed');
+    // Toggle the input type between 'password' and 'text'
+    if (passwordInput.type === 'password') {
+        eyeOpen.style.display = 'block';
+        eyeClosed.style.display = 'none';
+    }
+    else {
+        eyeOpen.style.display = 'none';
+        eyeClosed.style.display = 'block';
+    }
+    passwordInput.type = (passwordInput.type === 'password') ? 'text' : 'password';
+    // change eyeclosed to dsiplay None and eyeopen to display block
+}
