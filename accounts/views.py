5b7652ed9ad9cab9656d17d42d7e9dbad9ae0565
Miguel <Miguellozano3757@gmail.com>
2024-01-20 16:41:22 -0700

    added login logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,32 @@
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .forms import RegistrationForm
+from django.contrib import messages
+
 
 # Create your views here.
 def login_page(request):
     login_page=True
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        messages.info(request, 'username or password is incorrect')
+        
     context = {'login_page':login_page}
     return render(request, 'accounts/login.html',context)
 def register_page(request):
     errors = None
     login_page=False
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -22,6 +36,9 @@ def register_page(request):
             return redirect('home')
         errors = form.errors
         print(errors)
-    form = UserCreationForm()
+    form = RegistrationForm()
     context = {'login_page':login_page,'form': form,'errors':errors}
     return render(request, 'accounts/login.html',context)
+def logout_page(request):
+    logout(request)
+    return redirect('accounts:login')
--- firstapp/views.py
+++ firstapp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     context = {}
+    if request.user.is_authenticated== False:
+        return redirect('accounts:login')
     return render(request, 'firstapp/home.html',context)
