eb3f57045b7c2b4283f3ad50731b2917833d6196
Miguel <Miguellozano3757@gmail.com>
2024-08-01 13:29:29 -0700

    finished styling for first dash container

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -179,8 +179,6 @@ function closeNote() {
   clearNoteData();
 }
 //CODE FOR SUBMISSION VERIFICATION GOES HERE (THERE IS MORE IN VIEWS)//
-//write code that gives an error when a note tries to be submitted that has a time value but no date value, it would then prevent the form submission it should still the possibility for a date value but no time value//
-
 noteForm.addEventListener("submit", function (event) {
   if (exampleTimePickable.value && !input_date.value) {
     alert("Error: A note cannot have a time value without a date value.");
